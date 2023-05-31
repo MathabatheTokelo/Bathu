034a06582df5aa1a6c5a0e47b1171bc01009be37
Tokelo Mathabathe <Mathabathetokelo75@gmail.com>
2023-05-31 22:15:34 +0200

    login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -25,90 +25,89 @@ class LoginPage extends StatelessWidget {
         ),
         elevation: 10,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            CustomAppBar(),
-            SizedBox(
-              height: 150,
+      body: Column(
+        children: [
+          CustomAppBar(),
+          SizedBox(
+            height: 130,
+          ),
+          Text(
+            "LOGIN",
+            style: const TextStyle(
+              fontFamily: 'Gilroy-Bold',
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              letterSpacing: 2,
             ),
-            Text(
-              "LOGIN",
-              style: const TextStyle(
-                fontFamily: 'Gilroy-Regular.ttf',
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                letterSpacing: 5,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "Please enter your e-mail and password:",
+            style: const TextStyle(
+              fontFamily: 'Gilroy-Bold',
+              fontWeight: FontWeight.bold,
+              fontSize: 13,
+              letterSpacing: 0,
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            width: 310,
+            height: 40,
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                  labelText: "Email",
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  fillColor: Colors.grey,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(0.0))),
+                  labelStyle: TextStyle(
+                      color: Color.fromARGB(255, 99, 96, 96),
+                      fontFamily: 'Gilroy-Bold',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                      letterSpacing: 0)),
+              style: TextStyle(
+                color: Colors.grey,
               ),
             ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              "Please enter your e-mail and password:",
-              style: const TextStyle(
-                fontFamily: 'Gilroy-Regular.ttf',
-                fontWeight: FontWeight.bold,
-                fontSize: 12,
-                letterSpacing: 0,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: 300,
-              height: 40,
-              child: TextField(
-                autocorrect: true,
-                decoration: InputDecoration(
-                    labelText: "Email",
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 10,
-                    ),
-                    fillColor: Colors.grey,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(0.0))),
-                    labelStyle: TextStyle(
-                        color: Color.fromARGB(255, 129, 126, 126),
-                        fontFamily: 'Gilroy-Bold',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        letterSpacing: 0)),
-                style: TextStyle(
-                  color: Colors.grey,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: 310,
+            height: 40,
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 10,
                 ),
+                fillColor: Colors.black,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(0.0))),
+                labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 99, 96, 96),
+                    fontFamily: 'Gilroy-Bold',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                    letterSpacing: 0),
+              ),
+              style: TextStyle(
+                color: Colors.grey,
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: 300,
-              height: 40,
-              child: TextField(
-                autocorrect: true,
-                decoration: InputDecoration(
-                    labelText: "Password",
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 10,
-                    ),
-                    fillColor: Colors.black,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(0.0))),
-                    labelStyle: TextStyle(
-                        color: Color.fromARGB(255, 129, 126, 126),
-                        fontFamily: 'Gilroy-Bold',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        letterSpacing: 0)),
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
