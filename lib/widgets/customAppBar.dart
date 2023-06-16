d778ff402e93a8a0f5a9c991fac2f5c5dd2be3ec
Tokelo Mathabathe <Mathabathetokelo75@gmail.com>
2023-06-16 20:24:57 +0200

    Login dialog fix

--- lib/widgets/customAppBar.dart
+++ lib/widgets/customAppBar.dart
@@ -28,7 +28,9 @@ class _CustomAppBarState extends State<CustomAppBar> {
               InkWell(
                 child: Icon(Icons.menu, color: Colors.grey, size: 25),
                 onTap: () {
-                  Scaffold.of(context).openDrawer();
+                  {
+                    Scaffold.of(context).openDrawer();
+                  }
                 },
               ),
             ],
@@ -90,6 +92,178 @@ class _CustomAppBarState extends State<CustomAppBar> {
   }
 }
 
+void aleReg(var context) {
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.transparent,
+          insetPadding: EdgeInsets.all(10),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 350,
+                height: 320,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.white),
+                padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text(
+                        "REGISTER",
+                        style: TextStyle(
+                          color: Color(0xff1c1b1b),
+                          fontSize: 18,
+                          letterSpacing: 0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Gilroy-Bold',
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Please fill in the information below:",
+                        style: TextStyle(
+                          fontSize: 13,
+                          letterSpacing: .3,
+                          color: Color(0xff1c1b1b),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Theme(
+                        data: Theme.of(context).copyWith(
+                            textSelectionTheme: TextSelectionThemeData(
+                                selectionHandleColor: Colors.black,
+                                selectionColor: Colors.black)),
+                        child: SizedBox(
+                          width: 335,
+                          height: 50,
+                          child: TextFormField(
+                            cursorColor: Color(0xff1c1b1b),
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(0)),
+                                label: Text(
+                                  "First Name",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 0,
+                                      fontFamily: 'Gilroy-Regular.ttf'),
+                                )),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Theme(
+                        data: Theme.of(context).copyWith(
+                            textSelectionTheme: TextSelectionThemeData(
+                                selectionHandleColor: Colors.black,
+                                selectionColor: Colors.black)),
+                        child: SizedBox(
+                          width: 335,
+                          height: 50,
+                          child: TextFormField(
+                            cursorColor: Color(0xff1c1b1b),
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(0)),
+                                label: Text(
+                                  "Second Name",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 0,
+                                      fontFamily: 'Gilroy-Regular.ttf'),
+                                )),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Theme(
+                        data: Theme.of(context).copyWith(
+                            textSelectionTheme: TextSelectionThemeData(
+                                selectionHandleColor: Colors.black,
+                                selectionColor: Colors.black)),
+                        child: SizedBox(
+                          width: 335,
+                          height: 50,
+                          child: TextFormField(
+                            cursorColor: Color(0xff1c1b1b),
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(0)),
+                                label: Text(
+                                  "Email",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 0,
+                                      fontFamily: 'Gilroy-Regular.ttf'),
+                                )),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Theme(
+                        data: Theme.of(context).copyWith(
+                            textSelectionTheme: TextSelectionThemeData(
+                                selectionHandleColor: Colors.black,
+                                selectionColor: Colors.black)),
+                        child: SizedBox(
+                          width: 335,
+                          height: 50,
+                          child: TextFormField(
+                            cursorColor: Color(0xff1c1b1b),
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(0)),
+                                label: Text(
+                                  "Password",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 0,
+                                      fontFamily: 'Gilroy-Regular.ttf'),
+                                )),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xff1c1b1b),
+                            shape: BeveledRectangleBorder(
+                              borderRadius: BorderRadius.circular(1),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 128.0, vertical: 5.0),
+                          ),
+                          child: Text(
+                            "CREATE MY ACCOUNT",
+                            style: const TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 2,
+                                fontFamily: 'Gilroy-Regular.ttf'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      });
+}
+
 void aleLog(var context) {
   showDialog(
       context: context,
@@ -114,10 +288,46 @@ void aleLog(var context) {
                         "LOGIN",
                         style: TextStyle(
                           color: Colors.black,
-                          fontSize: 20,
-                          letterSpacing: 0,
+                          fontSize: 18,
+                          letterSpacing: 2,
                           fontWeight: FontWeight.bold,
                           fontFamily: 'Gilroy-Bold',
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Please enter your e-mail and password:",
+                        style: TextStyle(
+                          fontSize: 13,
+                          letterSpacing: .3,
+                          color: Color(0xff1c1b1b),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Theme(
+                        data: Theme.of(context).copyWith(
+                            textSelectionTheme: TextSelectionThemeData(
+                                selectionHandleColor: Colors.black,
+                                selectionColor: Colors.black)),
+                        child: SizedBox(
+                          width: 335,
+                          height: 50,
+                          child: TextFormField(
+                            cursorColor: Colors.black,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(0)),
+                              label: Text(
+                                "Email",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 0,
+                                    fontFamily: 'Gilroy-Regular.ttf'),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                       SizedBox(
@@ -128,40 +338,75 @@ void aleLog(var context) {
                             textSelectionTheme: TextSelectionThemeData(
                                 selectionHandleColor: Colors.black,
                                 selectionColor: Colors.black)),
-                        child: TextFormField(
-                          cursorColor: Colors.black,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(0)),
-                              label: Text(
-                                "Email",
-                              )),
+                        child: SizedBox(
+                          width: 335,
+                          height: 50,
+                          child: TextFormField(
+                            cursorColor: Color(0xff1c1b1b),
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(0)),
+                                label: Text(
+                                  "Password",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 0,
+                                      fontFamily: 'Gilroy-Regular.ttf'),
+                                )),
+                          ),
                         ),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Theme(
-                        data: Theme.of(context).copyWith(
-                            textSelectionTheme: TextSelectionThemeData(
-                                selectionHandleColor: Colors.black,
-                                selectionColor: Colors.black)),
-                        child: TextFormField(
-                          cursorColor: Colors.black,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(0)),
-                              label: Text("Password")),
+                      SizedBox(height: 15),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xff1c1b1b),
+                            shape: BeveledRectangleBorder(
+                              borderRadius: BorderRadius.circular(1),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 128.0, vertical: 15.0),
+                          ),
+                          child: Text(
+                            "LOGIN",
+                            style: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 2,
+                                fontFamily: 'Gilroy-Regular.ttf'),
+                          ),
                         ),
                       ),
-                      SizedBox(height: 20),
-                      InkWell(
-                        onTap: () {},
-                        child: Text("Don't have a Account? Register",
-                            style: TextStyle(color: Colors.blue)),
+                      SizedBox(height: 15),
+                      Center(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Don't have a account? ",
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  letterSpacing: .3,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  aleReg(context);
+                                },
+                                child: Text("Create on",
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      letterSpacing: .3,
+                                      color: Color(0xff1c1b1b),
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                            ]),
                       ),
-                      SizedBox(height: 10),
-                      Center(child: Button(st: "LOGIN"))
                     ],
                   ),
                 ),
@@ -170,86 +415,4 @@ void aleLog(var context) {
           ),
         );
       });
-
-  void aleReg(var context) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return Dialog(
-            backgroundColor: Colors.transparent,
-            insetPadding: EdgeInsets.all(10),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                SingleChildScrollView(
-                  child: Container(
-                    width: 350,
-                    height: 320,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Colors.white),
-                    padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
-                    child: Column(
-                      children: [
-                        Text(
-                          "LOGIN",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                            letterSpacing: 0,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Gilroy-Bold',
-                          ),
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Theme(
-                          data: Theme.of(context).copyWith(
-                              textSelectionTheme: TextSelectionThemeData(
-                                  selectionHandleColor: Colors.black,
-                                  selectionColor: Colors.black)),
-                          child: TextFormField(
-                            cursorColor: Colors.black,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(0)),
-                                label: Text(
-                                  "Email",
-                                )),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Theme(
-                          data: Theme.of(context).copyWith(
-                              textSelectionTheme: TextSelectionThemeData(
-                                  selectionHandleColor: Colors.black,
-                                  selectionColor: Colors.black)),
-                          child: TextFormField(
-                            cursorColor: Colors.black,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(0)),
-                                label: Text("Password")),
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        InkWell(
-                          onTap: () {},
-                          child: Text("Don't have a Account? Register",
-                              style: TextStyle(color: Colors.blue)),
-                        ),
-                        SizedBox(height: 10),
-                        Center(child: Button(st: "LOGIN"))
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        });
-  }
 }
